25fdca4a26eb5491ef848729e34fba7f99afa2e3
Bingapore <89854361+Bingapore@users.noreply.github.com>
2020-07-24 19:31:50 +0000

    Выпуск

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -1,2 +1,4 @@
 // Protractor configuration file, see link for more information
-// https://github.com/angular/protractor/blob/mas
+// https://github.com/angular/protractor/blob/master/lib/config.ts
+
+const { SpecReporter } = require('jasmine-spec-r
