6724895dbe86c48f65fe00e9fab59c7746c9e5e9
Bingapore <89854361+Bingapore@users.noreply.github.com>
2020-07-25 12:07:29 +0000

    Update some links

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -1,4 +1,9 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-r
+const { SpecReporter } = require('jasmine-spec-reporter');
+
+exports.config = {
+  allScriptsTimeout: 11000,
+  specs: [
+    './src
