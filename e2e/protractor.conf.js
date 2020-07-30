d908692941ebaac1e55470029f08c3be6c195596
Bingapore <89854361+Bingapore@users.noreply.github.com>
2020-07-30 17:31:02 +0000

    Fixed some typo

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -9,4 +9,8 @@ exports.config = {
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome
+    'browserName': 'chrome'
+  },
+  directConnect: true,
+  baseUrl: 'http://localhost:4200/',
+  framework: 
