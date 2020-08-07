e463f91571d8f062cf4c069329f3433ded109e8a
Bingapore <89854361+Bingapore@users.noreply.github.com>
2020-08-07 13:15:01 +0000

    sync: Add synchronization framework

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,3 +1,9 @@
 import { AppPage } from './app.po';
 
-describe('workspace-project App',
+describe('workspace-project App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
