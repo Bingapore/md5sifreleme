a038895bbfb0ff1f0845fe8798dbfbbbd04b301f
Bingapore <89854361+Bingapore@users.noreply.github.com>
2021-01-26 13:50:16 +0000

    [maven-release-plugin] prepare release v1.3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,4 +2,5 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { TestingPartComponent } from './testing-part/testing-part.component';
-import { Hom
+import { HomePageComponent } from './home-page/home-page.component';
+import { Regi
