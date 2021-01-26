bb7f642b009bf5ae72d4d74c4238ca15d0b8b358
Bingapore <89854361+Bingapore@users.noreply.github.com>
2021-01-26 13:22:55 +0000

    Adding travis config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { TestingPartCo
+import { TestingPartComponent } from './testing-part/testing-part.component';
+import { Hom
