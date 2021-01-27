2b5e09c78045831128b1ac5bbe22748f44f93898
Bingapore <89854361+Bingapore@users.noreply.github.com>
2021-01-27 15:16:06 +0000

    Update README.md again

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,4 +3,5 @@ import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { TestingPartComponent } from './testing-part/testing-part.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import { RegistrationPageComponent } from './registration-page/registrat
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
+import { BuyBitCoinsComponent } from './buy-bit-coins/buy-bit-coin
