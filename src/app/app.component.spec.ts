d9c50cb57ca998505d0fce3afa938d2b55754fea
Bingapore <89854361+Bingapore@users.noreply.github.com>
2021-03-13 16:22:52 +0000

    Update travis config.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -6,4 +6,7 @@ describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-  
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComp
