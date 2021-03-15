7ca9a56a3e427e638a56376fde79ac5cc62348be
Bingapore <89854361+Bingapore@users.noreply.github.com>
2021-03-15 12:15:52 +0000

    Fixes #77

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,4 +16,6 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = f
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  })
