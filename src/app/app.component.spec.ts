0b87885b3f3dbc592b617eaa1d7270bd14d78a89
Bingapore <89854361+Bingapore@users.noreply.github.com>
2021-03-21 11:09:01 +0000

    Fix header comment

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,4 +22,8 @@ describe('AppComponent', () => {
 
   it(`should have as title 'first-web-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.c
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('first-web-app');
+  });
+
+  i
