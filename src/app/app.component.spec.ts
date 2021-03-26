9f7cd12bc793700b22022a8cbcfd0660e43648f3
Bingapore <89854361+Bingapore@users.noreply.github.com>
2021-03-26 05:37:36 +0000

    Update from Forestry.io Zoltán Bába updated _data/changelog.yml

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,4 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to first-web-a
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to first-web-app!');
+  });
+});
