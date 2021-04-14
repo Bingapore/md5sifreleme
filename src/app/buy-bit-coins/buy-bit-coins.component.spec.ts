43a8fa1c0310d8593d3015b6b5ada09fcb9bee1c
Bingapore <89854361+Bingapore@users.noreply.github.com>
2021-04-14 14:11:51 +0000

    remove ignore

--- src/app/buy-bit-coins/buy-bit-coins.component.spec.ts
+++ src/app/buy-bit-coins/buy-bit-coins.component.spec.ts
@@ -14,4 +14,5 @@ describe('BuyBitCoinsComponent', () => {
   }));
 
   beforeEach(() => {
-    fixture = TestB
+    fixture = TestBed.createComponent(BuyBitCoinsComponent);
+    component = fixture.c
