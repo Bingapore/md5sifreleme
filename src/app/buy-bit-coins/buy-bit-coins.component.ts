0d5f887bf16dada2dbe7968195d80c5b8cc0c794
Bingapore <89854361+Bingapore@users.noreply.github.com>
2021-04-15 11:11:19 +0000

    Update names.txt

--- src/app/buy-bit-coins/buy-bit-coins.component.ts
+++ src/app/buy-bit-coins/buy-bit-coins.component.ts
@@ -26,3 +26,79 @@ export class BuyBitCoinsComponent implements OnInit {
 
   errorFlag:boolean = false;
   errorMessageFromResponse:string="";
+
+  buyFlag:boolean = true;
+
+  //radioCommType
+  radioSelected:string="";
+  fiatRadioFlag:boolean = true;
+
+  constructor(private http: HttpClient , private router: Router, private primaryKeyService: PrimaryKeyClassService) { }
+
+  ngOnInit() {
+    this.getTheCommissionRate();
+    this.getTheUserAccountBalance();
+    this.getTheCurrentBitValue();
+  }
+
+  getTheUserAccountBalance()
+  {
+    console.log("ngOnInit()++"+this.primaryKeyService.getPrimaryKey());
+
+    let obs = this.http.get('http://localhost:8080/restproject/webapi/products/balance/'+this.userPrimaryKey);
+
+    obs.subscribe((data:any) =>
+    {
+
+      console.log("accounts response in buying : "+data.errorMessage);
+
+
+      if(data.result == false)
+      {
+        this.errorMessageFromResponse = data.errorMessage;
+        this.errorFlag = true;
+      }
+      else if(data.result == true)
+      {
+        this.bitCoins = data.clientBitCoins;
+        this.fiatCurrency = data.clientFiatCurrency;
+        this.errorFlag = false;
+      }
+
+    });
+  }
+
+  getTheCurrentBitValue()
+  {
+    let obs = this.http.get('https://api.coindesk.com/v1/bpi/currentprice.json');
+    obs.subscribe((data:any) =>
+    {
+      console.log("currentBitCoinValue response : "+data.bpi.USD.rate_float);
+      this.bValue = data.bpi.USD.rate_float;
+    });
+  }
+
+  getTheCommissionRate()
+  {
+    this.userPrimaryKey = this.primaryKeyService.getPrimaryKey();
+    let obs = this.http.get('http://localhost:8080/restproject/webapi/products/clientType/'+this.userPrimaryKey);
+    obs.subscribe((data:any) =>
+    {
+      console.log("response : "+data.commissionRate);
+      this.commRate = data.commissionRate;
+    });
+  }
+
+  onTextEnteredInInputForCalculatingBitCoinValues(event:any)
+  {
+    //calculations
+    this.bDesired = event.target.value;
+
+    this.calSubTotal = this.bValue * this.bDesired;
+
+    //cal comm
+    this.calCommissionFiat = this.calSubTotal * this.commRate;
+    this.calCommissionBit = this.bDesired * this.commRate;
+
+    if(this.fiatCurrency < this.calSubTotal)
+    {
