ab04d1abfba7930355e25c6e8c8037237bd88416
Bingapore <89854361+Bingapore@users.noreply.github.com>
2021-05-07 12:15:30 +0000

    MINIFY

--- src/app/child-names/child-names.component.ts
+++ src/app/child-names/child-names.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input,Output,EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-child-na
+  selector: 'app-child-names',
+  templateUrl: './child-names.component.html',
+  styleUrls: ['./child-names
