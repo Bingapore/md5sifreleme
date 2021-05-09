03e16f3def1ff71c7c1605a5f1d15d320d860e04
Bingapore <89854361+Bingapore@users.noreply.github.com>
2021-05-09 09:37:44 +0000

    chore: add changelog

--- src/app/child-names/child-names.component.ts
+++ src/app/child-names/child-names.component.ts
@@ -3,4 +3,6 @@ import { Component, OnInit, Input,Output,EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-child-names',
   templateUrl: './child-names.component.html',
-  styleUrls: ['./child-names
+  styleUrls: ['./child-names.component.css']
+})
+export class ChildNamesComponent implement
