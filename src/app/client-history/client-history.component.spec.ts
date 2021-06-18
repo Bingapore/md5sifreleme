b19b2c819c0439a37c347605bad921a07e4c22e5
Bingapore <89854361+Bingapore@users.noreply.github.com>
2021-06-18 22:15:35 +0000

    Checkpointing TypeChecker.

--- src/app/client-history/client-history.component.spec.ts
+++ src/app/client-history/client-history.component.spec.ts
@@ -2,4 +2,5 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ClientHistoryComponent } from './client-history.component';
 
-des
+describe('ClientHistoryComponent', () => {
+  let component: Client
