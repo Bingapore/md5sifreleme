4d7b92e38d5ede007863ceda7f2a71a41fc34b36
Bingapore <89854361+Bingapore@users.noreply.github.com>
2021-06-26 18:39:53 +0000

    New data for roulette

--- src/app/client-history/client-history.component.spec.ts
+++ src/app/client-history/client-history.component.spec.ts
@@ -3,4 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ClientHistoryComponent } from './client-history.component';
 
 describe('ClientHistoryComponent', () => {
-  let component: Client
+  let component: ClientHistoryComponent;
+  let fixture: ComponentFixture<ClientHistoryComponent>;
+
+  
