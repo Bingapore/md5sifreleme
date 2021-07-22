3728034526d227c8511e1a546e31090553ba2fca
Bingapore <89854361+Bingapore@users.noreply.github.com>
2021-07-22 16:38:47 +0000

    use eslint

--- src/app/client-history/client-history.component.ts
+++ src/app/client-history/client-history.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { PrimaryKeyClassService } from '../primary-key-class.service';
 import { HttpClient } from '@angular/common/http';
-import
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-clie
