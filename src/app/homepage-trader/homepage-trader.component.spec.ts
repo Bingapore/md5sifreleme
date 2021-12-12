debdd0ab99cd77a2f6c880844756da510bed5902
Bingapore <89854361+Bingapore@users.noreply.github.com>
2021-12-12 08:16:44 +0000

    Dist build.

--- src/app/homepage-trader/homepage-trader.component.spec.ts
+++ src/app/homepage-trader/homepage-trader.component.spec.ts
@@ -2,4 +2,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomepageTraderComponent } from './homepage-trader.component';
 
-describe('Homepa
+describe('HomepageTraderComponent', () => {
+  let component: HomepageTraderComponent;
+  let fi
