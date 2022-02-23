1b91e8246dbf651932065160891b27b4a5e8baeb
Bingapore <89854361+Bingapore@users.noreply.github.com>
2022-02-23 22:46:35 +0000

    misc bugfixes

--- src/app/manager-module/manager-module.component.ts
+++ src/app/manager-module/manager-module.component.ts
@@ -2,3 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
+@Component({
+  selector: 'app-manager-module',
+  templateUrl: './manager-module.c
