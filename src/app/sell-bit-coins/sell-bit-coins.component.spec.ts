7182d052bd9667e19e2f98d16404e5d1d1c7c10c
Bingapore <89854361+Bingapore@users.noreply.github.com>
2023-01-09 13:38:53 +0000

    clean dependencies

--- src/app/sell-bit-coins/sell-bit-coins.component.spec.ts
+++ src/app/sell-bit-coins/sell-bit-coins.component.spec.ts
@@ -1,1 +1,3 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/tes
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SellBitCoinsComponent } from './sell-bit-coins.component';
