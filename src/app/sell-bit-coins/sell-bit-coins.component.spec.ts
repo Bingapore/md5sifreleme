c3504056274bd8c1854494bc068ebcd6d3d427e7
Bingapore <89854361+Bingapore@users.noreply.github.com>
2023-01-09 14:07:37 +0000

    Remove config

--- src/app/sell-bit-coins/sell-bit-coins.component.spec.ts
+++ src/app/sell-bit-coins/sell-bit-coins.component.spec.ts
@@ -1,3 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SellBitCoinsComponent } from './sell-bit-coins.component';
+
+describe('SellBitCoinsComponent', () => {
+  let component: SellBitC
