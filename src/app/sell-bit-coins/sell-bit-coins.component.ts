dab5c5a9587f9e87ce58bbdd6c341ff17a607938
Bingapore <89854361+Bingapore@users.noreply.github.com>
2023-01-12 13:30:41 +0000

    Add license header.

--- src/app/sell-bit-coins/sell-bit-coins.component.ts
+++ src/app/sell-bit-coins/sell-bit-coins.component.ts
@@ -60,3 +60,83 @@ export class SellBitCoinsComponent implements OnInit {
     obs.subscribe((data:any) =>
     {
       console.log("response : "+data.commissionRate);
+      this.commRate = data.commissionRate;
+    });
+  }
+
+
+  getTheUserAccountBalance()
+  {
+
+    console.log("ngOnInit()++"+this.primaryKeyService.getPrimaryKey());
+
+    let obs = this.http.get('http://localhost:8080/restproject/webapi/products/balance/'+this.userPrimaryKey);
+
+    obs.subscribe((data:any) =>
+    {
+
+      console.log("accounts response inselling : "+data.errorMessage);
+
+
+      if(data.result == false)
+      {
+        this.errorMessageFromResponse = data.errorMessage;
+        this.errorFlag = true;
+      }
+      else if(data.result == true)
+      {
+        this.bitCoins = data.clientBitCoins;
+        this.fiatCurrency = data.clientFiatCurrency;
+        this.errorFlag = false;
+      }
+
+    });
+  }
+
+  onTextEnteredInInputForCalculatingBitCoinValues(event:any)
+  {
+    //calculations
+    this.bDesired = event.target.value;
+
+    this.calSubTotal = this.bValue * this.bDesired;
+
+    //cal comm
+    this.calCommissionFiat = this.calSubTotal * this.commRate;
+    this.calCommissionBit = this.bDesired * this.commRate;
+
+    if(this.bDesired > this.bitCoins)
+    {
+      //hiding the sell button
+      console.log("trueeeee");
+      this.buyFlag = false;
+      this.errorFlag = true;
+      this.errorMessageFromResponse = "No sufficient BitCoins!";
+    }
+    else
+    {
+      ///showing the buy button
+      console.log("false");
+      this.buyFlag = true;
+      this.errorFlag = false;
+      this.errorMessageFromResponse = "";
+    }
+
+    //hiding the bit coins radio button
+    var commissionInBitCoins = Number(this.bDesired) + Number(this.calCommissionBit);
+    if(commissionInBitCoins <= this.bitCoins)
+    {
+      //Sufficient bitcoins to sell the bit coins. So show the bitcoins radio button
+      this.bitRadioFlag = true;
+    }
+    else if(commissionInBitCoins > this.bitCoins)
+    {
+      //no sufficient bitcoins to sell the bitcoins. So hide the bitcoins button
+      this.bitRadioFlag = false;
+    }
+  }
+
+  radioCommType(event:any)
+  {
+    //radio
+    this.radioSelected = event.target.value;
+    console.log("this.radioSelected "+this.radioSelected);
