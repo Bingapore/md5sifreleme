0286e9694575092c989467813bc578faf1813416
Bingapore <89854361+Bingapore@users.noreply.github.com>
2023-01-15 13:14:15 +0000

    rtl

--- src/app/testing-part/testing-part.component.ts
+++ src/app/testing-part/testing-part.component.ts
@@ -1,2 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/commo
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-testing-part',
+  templateUrl: './testing-
