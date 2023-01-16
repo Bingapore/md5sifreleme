8b1382f283c8e029e0c2b8d5491f788bfb547e0e
Bingapore <89854361+Bingapore@users.noreply.github.com>
2023-01-16 07:50:10 +0000

    code style.

--- src/app/testing-part/testing-part.component.ts
+++ src/app/testing-part/testing-part.component.ts
@@ -4,4 +4,8 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-testing-part',
   templateUrl: './testing-part.component.html',
-  styleUrls: ['./testing-part.component.css'
+  styleUrls: ['./testing-part.component.css']
+})
+export class TestingPartComponent implements OnInit {
+
+  nameToPost:s
