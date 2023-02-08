b644d32e777ead95f238c58cc0be8c3836028fe9
Bingapore <89854361+Bingapore@users.noreply.github.com>
2023-02-08 10:47:08 +0000

    Update title.

--- src/app/trader-child-buy/trader-child-buy.component.spec.ts
+++ src/app/trader-child-buy/trader-child-buy.component.spec.ts
@@ -19,4 +19,7 @@ describe('TraderChildBuyComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create'
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/trader-child-buy/trader-child-buy.component.ts
@@ -0,0 +1,5 @@
+
+import { Component, OnInit, Input } from '@angular/core';
+import { PrimaryKeyClassService } from '../primary-key-class.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
