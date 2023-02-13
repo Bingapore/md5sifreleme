875ed5c28b517ae08f9ff3fdfbc66ec150a2e7a6
Bingapore <89854361+Bingapore@users.noreply.github.com>
2023-02-13 19:11:41 +0000

    Removed the .idea folder

---
+++ src/app/trader-childs/trader-childs.component.spec.ts
@@ -0,0 +1,8 @@
+
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TraderChildsComponent } from './trader-childs.component';
+
+describe('TraderChildsComponent', () => {
+  let component: TraderChildsComponent;
+  let fixture: ComponentFixture<TraderChildsComponent>;
