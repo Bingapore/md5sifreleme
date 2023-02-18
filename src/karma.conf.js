8a93a496a4c5180fec2d6ea277013504b2db5ac2
Bingapore <89854361+Bingapore@users.noreply.github.com>
2023-02-18 14:28:39 +0000

    Removed unused styles

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -4,4 +4,7 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma
