203aaa0f467bfd9ef5c356ce49981ae3bfcc469b
Bingapore <89854361+Bingapore@users.noreply.github.com>
2023-02-24 20:13:23 +0000

    Update for new release

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,4 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from 
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.mo
