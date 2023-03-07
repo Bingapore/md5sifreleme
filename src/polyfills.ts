ffb9336af57eacd9d7e7d1e33f79085b2278d5d5
Bingapore <89854361+Bingapore@users.noreply.github.com>
2023-03-07 06:34:42 +0000

    Release version 1.2

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -42,4 +42,5 @@
 // import 'core-js/es6/array';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
-//
+// import 'classlist.js';  // Run `npm install --save classlist.js`.
+
