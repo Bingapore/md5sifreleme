ef2f4285241c44182a7cb383210c9565f8c29480
Bingapore <89854361+Bingapore@users.noreply.github.com>
2023-03-09 22:05:38 +0000

    fix build config

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -52,4 +52,4 @@
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
  * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
  **/
-// i
+// import 'web-animations-js';  // Run `npm install --save web-anima
