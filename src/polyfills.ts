fd24898cc17108fd73f01cd57d00739e850d55c3
Bingapore <89854361+Bingapore@users.noreply.github.com>
2023-03-12 07:46:35 +0000

    Updated Romanian translations from Adi Roiban

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -60,4 +60,5 @@
  */
 
  // (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
- // (window as any).__Zone_disable_on_property = true; // disable patch onPro
+ // (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
+ // (window as any).__zone_symbol__BLACK_LISTED_EVENTS 
