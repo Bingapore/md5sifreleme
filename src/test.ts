51607ea2812dfe40e1ea398422495ad37285bca4
Bingapore <89854361+Bingapore@users.noreply.github.com>
2023-03-17 19:01:50 +0000

    0.17.1

--- src/test.ts
+++ src/test.ts
@@ -13,3 +13,6 @@ declare const require: any;
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
+);
+// Then we find all the tests.
+const context = require.context('./', 
